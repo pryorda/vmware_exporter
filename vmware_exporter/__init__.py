aeccb035d368dcc8e6bc52628d7eef786345725b
Remi Verchere <remi@verchere.fr>
2018-01-31 21:09:07 +0100

    Set default encoding to UTF-8

--- vmware_exporter/__init__.py
+++ vmware_exporter/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Remi Verchere"
 __license__ = "BSD 3-Clause License"
--- vmware_exporter/vmware_exporter.py
+++ vmware_exporter/vmware_exporter.py
@@ -442,4 +442,6 @@ def main():
 
 
 if __name__ == '__main__':
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
     main()
