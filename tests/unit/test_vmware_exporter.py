fb23c64c2020b728c04963037be5037bdcdd6426
Frank Lin PIAT <fpiat@klabs.be>
2021-07-24 01:12:46 +0200

    implement --version option

--- tests/unit/test_vmware_exporter.py
+++ tests/unit/test_vmware_exporter.py
@@ -1698,3 +1698,11 @@ def test_valid_loglevel_cli_argument():
 def test_main():
     with pytest.raises(SystemExit):
         main(['-h', '-l debug'])
+
+
+def test_version(capsys):
+    with pytest.raises(SystemExit):
+        main(['-v'])
+    captured = capsys.readouterr()
+    assert captured.out.startswith("vmware_exporter")
+    assert captured.err == ""
